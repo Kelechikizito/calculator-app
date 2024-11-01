5a91f3c69cfa50d8894fc5b9d5f9570f222e976f
Kelechikizito <kaylaychi77@gmail.com>
2024-11-01 12:14:02 +0100

    files update

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,7 +4,7 @@ const Buttons = () => {
       <button data-num className="rounded-lg px-8 py-2 bg-[#e9e2d9] text-[#444B5A]">7</button>
       <button data-num className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">8</button>
       <button data-num className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">9</button>
-      <button data-delete className="rounded-lg bg-[#637097] text-[#e9e2d9]">DEL</button>
+      <button data-delete className="rounded-lg bg-[#637097] text-[#EAE3DC]">DEL</button>
       <button data-num className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">4</button>
       <button data-num className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">5</button>
       <button data-num className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">6</button>
@@ -17,7 +17,7 @@ const Buttons = () => {
       <button data-num className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">0</button>
       <button data-operator className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">/</button>
       <button data-operator className="rounded-lg bg-[#e9e2d9] text-[#444B5A]">x</button>
-      <button data-reset className="col-span-2 rounded-lg bg-[#637097] text-[#e9e2d9]">
+      <button data-reset className="col-span-2 rounded-lg bg-[#637097] text-[#EAE3DC]">
         RESET
       </button>
       <button data-output className="col-span-2 rounded-lg bg-[#D03F2F] text-white">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,28 @@
 const Header = () => {
   return (
-    <header className="flex justify-between">
-        <div>
-            <h1>calc</h1>
+    <header className="flex justify-between text-[#EAE3DC]">
+      <div>
+        <h1 className="text-2xl font-bold">calc</h1>
+      </div>
+
+      <div className="flex gap-4">
+        <div className="flex items-center justify-center text-[0.6rem] font-bold tracking-wide">
+          <h6>THEME</h6>
         </div>
 
-        <div className="flex">
-            <h6>THEME</h6>       
+        <div className="input flex place-items-center">
+          <input id="theme1" type="radio" name="theme" value="0" checked />
+          <label htmlFor="theme1">1</label>
 
-            <div className="input">
-                <input id="theme1" type="radio" name="theme" value="0" checked/>
-                <label htmlFor="theme1">1</label>
-                
-                <input id="theme2" type="radio" value="1" name="theme"/>
-                <label htmlFor="theme2">2</label>
-                
-                <input id="theme3" type="radio" value="2" name="theme"/>
-                <label htmlFor="theme3">3</label>
-            </div>
+          <input id="theme2" type="radio" value="1" name="theme" />
+          <label htmlFor="theme2">2</label>
+
+          <input id="theme3" type="radio" value="2" name="theme" />
+          <label htmlFor="theme3">3</label>
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
