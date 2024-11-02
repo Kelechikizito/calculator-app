c94c5e932bfe239e789f4afca67716f790ebf00f
Kelechikizito <kaylaychi77@gmail.com>
2024-11-02 06:15:48 +0100

    files update

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -2,7 +2,7 @@ const UserInput = () => {
   return (
     <div className="bg-[#182034] h-24 p-2 px-4 rounded-lg text-white flex flex-col justify-center text-right font-bold">
       <div data-previous-operand className="previous-operand">111</div>
-      <div data-current-operand className="current-operand text-5xl sm:max-w-[360px] max-w-[293px] overflow-x-auto overflow-y-hidden whitespace-nowrap w-full">111,289,929,999,999,888,888</div>
+      <div data-current-operand className="current-operand text-5xl sm:max-w-[360px] max-w-[293px] overflow-x-auto overflow-y-hidden whitespace-nowrap w-full">111,289,929</div>
     </div>
   );
 };
