d8c90e9fcb1645e53235b1f7032f5ba22ea127e2
Kelechikizito <kaylaychi77@gmail.com>
2024-11-05 15:01:04 +0100

    styling update

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -12,126 +12,126 @@ const Buttons = ({ handleClick }) => {
     <div className="grid grid-cols-4 grid-rows-5 p-4 py-6 gap-4 bg-[#262b38] rounded-lg text-2xl font-bold">
       <button
         data-num
-        className="rounded-lg sm:px-8 px-6 py-2 bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg sm:px-8 px-6 py-2 bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         7
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         8
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         9
       </button>
       <button
         data-delete
-        className="rounded-lg bg-[#637097] text-[#EAE3DC] shadow-[0_4px_#3a516d]"
+        className="rounded-lg bg-[#637097] text-[#EAE3DC] shadow-[0_4px_#3a516d] active:scale-95"
         onClick={ClickHandler}
       >
         DEL
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         4
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         5
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         6
       </button>
       <button
         data-operator
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         +
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         1
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         2
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         3
       </button>
       <button
         data-operator
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         -
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         .
       </button>
       <button
         data-num
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         0
       </button>
       <button
         data-operator
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         /
       </button>
       <button
         data-operator
-        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895]"
+        className="rounded-lg bg-[#e9e2d9] text-[#444B5A] shadow-[0_4px_#b8a895] active:scale-95"
         onClick={ClickHandler}
       >
         x
       </button>
       <button
         data-reset
-        className="col-span-2 rounded-lg bg-[#637097] text-[#EAE3DC] shadow-[0_4px_#3a516d]"
+        className="col-span-2 rounded-lg bg-[#637097] text-[#EAE3DC] shadow-[0_4px_#3a516d] active:scale-95"
         onClick={ClickHandler}
       >
         RESET
       </button>
       <button
         data-output
-        className="col-span-2 rounded-lg bg-[#D03F2F] text-white shadow-[0_4px_#8b2e24]"
+        className="col-span-2 rounded-lg bg-[#D03F2F] text-white shadow-[0_4px_#8b2e24] active:scale-95"
         onClick={ClickHandler}
       >
         =
